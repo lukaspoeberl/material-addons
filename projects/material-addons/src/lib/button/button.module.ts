69910cdb57297030f0196c482853e85f21e19e00
Fari Kiani <Farideh.Kianimougooei@cloudflight.io>
2023-03-14 18:23:46 +0100

    Add MaterialAddons Button and ButtonGroups

--- projects/material-addons/src/lib/button/button.module.ts
+++ projects/material-addons/src/lib/button/button.module.ts
@@ -1,18 +1,43 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { PrimaryButtonComponent } from './primary-button/primary-button.component';
-import { OutlineButtonComponent } from './outline-button/outline-button.component';
-import { LinkButtonComponent } from './flat-button/link-button.component';
-import { IconButtonComponent } from './icon-button/icon-button.component';
-import { DangerButtonComponent } from './danger-button/danger-button.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterModule} from '@angular/router';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {PrimaryButtonComponent} from './primary-button/primary-button.component';
+import {OutlineButtonComponent} from './outline-button/outline-button.component';
+import {LinkButtonComponent} from './flat-button/link-button.component';
+import {IconButtonComponent} from './icon-button/icon-button.component';
+import {DangerButtonComponent} from './danger-button/danger-button.component';
+import {MadButtonDirective} from './mad-button/mad-button.directive';
+import {MadButtonGroupComponent} from './mad-button-group/mad-button-group.component';
 
 @NgModule({
-  declarations: [PrimaryButtonComponent, OutlineButtonComponent, LinkButtonComponent, DangerButtonComponent, IconButtonComponent],
-  imports: [CommonModule, RouterModule, MatButtonModule, MatIconModule, MatTooltipModule],
-  exports: [PrimaryButtonComponent, OutlineButtonComponent, LinkButtonComponent, DangerButtonComponent, IconButtonComponent],
+  declarations: [
+    PrimaryButtonComponent,
+    OutlineButtonComponent,
+    LinkButtonComponent,
+    DangerButtonComponent,
+    IconButtonComponent,
+    MadButtonDirective,
+    MadButtonGroupComponent,
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTooltipModule
+  ],
+  exports: [
+    PrimaryButtonComponent,
+    OutlineButtonComponent,
+    LinkButtonComponent,
+    DangerButtonComponent,
+    IconButtonComponent,
+    MadButtonDirective,
+    MadButtonGroupComponent,
+  ],
 })
-export class ButtonModule {}
+export class ButtonModule {
+}
